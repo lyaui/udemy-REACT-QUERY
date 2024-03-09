a03e6536b70119c649308a034cc72ea9e3661818
Iris Huang <lyaui@outlook.com>
2024-03-10 02:49:02 +0800

    Custom Query Hook: useTreatments

--- base-lazy-days/client/src/components/app/Home.tsx
+++ base-lazy-days/client/src/components/app/Home.tsx
@@ -1,14 +1,14 @@
-import { Icon, Stack, Text } from "@chakra-ui/react";
-import { GiFlowerPot } from "react-icons/gi";
+import { Icon, Stack, Text } from '@chakra-ui/react';
+import { GiFlowerPot } from 'react-icons/gi';
 
-import { BackgroundImage } from "@/components/common/BackgroundImage";
+import { BackgroundImage } from '@/components/common/BackgroundImage';
 
 export function Home() {
   return (
-    <Stack textAlign="center" justify="center" height="84vh">
+    <Stack textAlign='center' justify='center' height='84vh'>
       <BackgroundImage />
-      <Text textAlign="center" fontFamily="Forum, sans-serif" fontSize="6em">
-        <Icon m={4} verticalAlign="top" as={GiFlowerPot} />
+      <Text textAlign='center' fontFamily='Forum, sans-serif' fontSize='6em'>
+        <Icon m={4} verticalAlign='top' as={GiFlowerPot} />
         Lazy Days Spa
       </Text>
       <Text>Hours: limited</Text>
--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,15 +1,23 @@
-import type { Treatment } from "@shared/types";
+import { useQuery } from '@tanstack/react-query';
 
-import { axiosInstance } from "@/axiosInstance";
-import { queryKeys } from "@/react-query/constants";
+import type { Treatment } from '@shared/types';
+
+import { axiosInstance } from '@/axiosInstance';
+import { queryKeys } from '@/react-query/constants';
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
+  const fallback: Treatment[] = [];
   // TODO: get data from server via useQuery
-  return [];
+  const { data = fallback } = useQuery({
+    queryKey: [queryKeys.treatments],
+    queryFn: getTreatments,
+  });
+  console.log({ data });
+  return data;
 }
